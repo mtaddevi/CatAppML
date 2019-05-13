75ce3fa5162d85a94019646bf6eefbe116127858
mtaddevi <mtaddevi@gmail.com>
2019-05-13 09:06:27 -0500

    Initial commit

--- CatML/ViewController.swift
+++ CatML/ViewController.swift
@@ -7,12 +7,79 @@
 //
 
 import UIKit
+import AVKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    @IBOutlet weak var cameraDisplay: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setUpCamera()
+        
+        
+    }
+    
+    func setUpCamera(){
+        guard let device = AVCaptureDevice.default(for: .video) else {return}
+        guard let input = try? AVCaptureDeviceInput(device: device) else {return}
+        let session = AVCaptureSession()
+        //bloew sets session quality to 4k
+        session.sessionPreset = .hd4K3840x2160
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.frame = view.frame
+        cameraDisplay.layer.addSublayer(previewLayer)
+        
+        let output = AVCaptureVideoDataOutput()
+        output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "CameraOutput"))
+        
+        session.addInput(input)
+        session.addOutput(output)
+        session.startRunning()
+    }
+    //from AVCapture delegate in class signature
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let sampleBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        scanImage(buffer: sampleBuffer)
+    }
+    
+    func scanImage(buffer: CVPixelBuffer){
+        //get ml model
+        guard let model = try? VNCoreMLModel(for: catML().model) else {return}
+        let request = VNCoreMLRequest(model: model) { request, _ in
+            guard let results = request.results as? [VNClassificationObservation] else {return}
+            guard let mostConfidentResult = results.first else {return}
+            
+            DispatchQueue.main.async {
+                if mostConfidentResult.confidence >= 0.95 {
+                    let confidenceText = "\n \(Int(mostConfidentResult.confidence * 100.0))% confidence"
+                
+                    switch mostConfidentResult.identifier {
+                        
+//                    case "appleWatch":self.resultLabel.text = "APPLE WATCH \(confidenceText)"
+                    case "snickers":self.resultLabel.text = "CAT\(confidenceText)"
+                        default: return
+                    }
+                }else{
+                    self.resultLabel.text = "Unknown object, need more data"
+                }
+                
+            }
+       
+            
+           
+        }
+        let requestHandler = VNImageRequestHandler(cvPixelBuffer: buffer, options: [:])
+        do{
+            try requestHandler.perform([request])
+        }catch{
+            print(error)
+        }
+        
+    
     }
 
 
